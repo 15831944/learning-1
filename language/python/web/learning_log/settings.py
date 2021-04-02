836395aef9cf0d480f8fecc64407f8c67c1cc9a0
gdimitriu <gabriel.dimitriu@yahoo.com>
2021-04-02 01:32:42 -0700

    removed secret key

--- language/python/web/learning_log/settings.py
+++ language/python/web/learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#1^4$y=3rh05%sr$(o)=q^7&4f24vow#=+q^%@*1+svz6dm^6h'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
